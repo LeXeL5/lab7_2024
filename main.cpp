f49162d1a60b02bb4577f18830bb95902405fa74
LeXeL5 <lexel_2001@bk.ru>
2024-05-04 01:50:25 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+using namespace std;
+
+enum Order { Prefix, Infix, Postfix };
+struct Node {
+	Node* left = nullptr;
+	Node* right = nullptr;
+	int value = 0;
+};
+struct Tree {
+	int size = 0;
+	Node* root = nullptr;
+	int req(int* array, int index, Order order, Node* current) {
+		if (current == nullptr) return index;
+		if (order == Prefix) {
+			array[index] = current->value;
+			index++;
+		}
+		index = req(array, index, order, current->left);
+		if (order == Infix) {
+			array[index] = current->value;
+			index++;
+		}
+		index = req(array, index, order, current->right);
+		if (order == Postfix) {
+			array[index] = current->value;
+			index++;
+		}
+	}
+	int* ToArray(Order order = Infix) {
+		int* array = new int[count()];
+		req(array, 0, order, root);
+		return array;
+	}
+	int subtreeHight(Node* node) {
+
+	}
+	Node* goTo(int value) {
+		Node* current = root;
+		while (current != nullptr) {
+			if (value == current->value) {
+				return current;
+			}
+			else if (value < current->value) {
+				current = current->left;
+			}
+			else {
+				current = current->right;
+			}
+		}
+		return nullptr;
+	}
+	void add(int value) {
+		Node* newNode = new Node;
+		newNode->value = value;
+		size++;
+		if (count() == 1) {
+			root = newNode;
+			return;
+		}
+		Node* current = root;
+		while (true) {
+			if (value < current->value) {
+				if (current->left != nullptr) {
+					current = current->left;
+				}
+				else {
+					current->left = newNode;
+					return;
+				}
+			}
+			else if (value > current->value) {
+				if (current->right != nullptr) {
+					current = current->right;
+				}
+				else {
+					current->right = newNode;
+					return;
+				}
+			}
+			else if (value == current->value) {
+				delete newNode;
+				size--;
+				return;
+			}
+		}
+	}
+	void remove(int value) {
+		Node* parent = nullptr;
+		Node* current = root;
+		while (current != nullptr) {
+			if (value == current->value) {
+				break;
+			}
+			parent = current;
+			if (value < current->value) {
+				current = current->left;
+			}
+			else {
+				current = current->right;
+			}
+		}
+		if (current == nullptr) return;
+		if ((current->right != nullptr) && (current->left != nullptr)) {
+			parent = current;
+			Node* min = current->right;
+			while (min->left != nullptr) {
+				parent = min;
+				min = min->left;
+			}
+			current->value = min->value;
+			current = min;
+		}
+		Node* child = nullptr;
+		if (current->left != nullptr) {
+			child = current->left;
+		}
+		else {
+			child = current->right;
+		}
+		if (current != root) {
+			if (parent->left == current) {
+				parent->left = child;
+			}
+			else if (parent->right == current) {
+				parent->right = child;
+			}
+		}
+		else {
+			root = child;
+		}
+		delete current;
+		size--;
+	}
+	void clear(Node* current) {
+		if (current == nullptr) return;
+		clear(current->left);
+		clear(current->right);
+		delete current;
+		size--;
+	}
+	void clear() {
+		clear(root);
+		root = nullptr;
+	}
+	bool contains(int value) {
+		if (goTo(value) != nullptr) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	int count() {
+		return size;
+	}
+};
+void main() {
+	Tree tree;
+	int input = 0;
+	cout << "Enter sequence of numbers (sequence end sign 0):" << endl;
+	cin >> input;
+	while (input) {
+		tree.add(input);
+		cin >> input;
+	}
+	int* arr = tree.ToArray();
+	for (int i = 0; i < tree.count(); i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "Enter numbers to search (sequence end sign 0):" << endl;
+	cin >> input;
+	while (input) {
+		if (tree.contains(input)) {
+			cout << "This number is on the tree" << endl;
+		}
+		else {
+			cout << "This number is NOT on the tree" << endl;
+		}
+		cin >> input;
+	}
+	tree.clear();
+}
