f7138433b484e8bdf322002204aef64a26938216
LeXeL5 <lexel_2001@bk.ru>
2024-05-18 15:44:17 +0300

    Version 7.4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-	Node* left = nullptr;
-	Node* right = nullptr;
-	int value = 0;
-};
+
 struct Tree {
+	struct Node {
+		Node* left = nullptr;
+		Node* right = nullptr;
+		int value = 0;
+	};
+	struct Heightmap {
+		Node* left = nullptr;
+		Node* right = nullptr;
+	};
+
 	enum Order { Prefix, Infix, Postfix };
 	int size = 0;
 	Node* root = nullptr;
+
 	int req(int* array, int index, Order order, Node* current) {
 		if (current == nullptr) return index;
 		if (order == Prefix) {
@@ -32,35 +39,29 @@ struct Tree {
 		req(array, 0, order, root);
 		return array;
 	}
+
 	int subtreeDepth(Node* current) {
 		if (current == nullptr) return 0;
 		int left = subtreeDepth(current->left);
 		int right = subtreeDepth(current->right);
 		return max(left, right) + 1;
 	}
-	int minLeafDepth(Node* current){
+	int minLeafDepth(Node* current) {
 		if (current == nullptr) return 0;
 		int left = minLeafDepth(current->left);
 		int right = minLeafDepth(current->right);
 		return min(left, right) + 1;
 	}
-	void ToLeft(int value) {
-		Node* parent = nullptr;
-		Node* current = root;
-		while (current != nullptr) {
-			if (value == current->value) {
-				break;
-			}
-			parent = current;
-			if (value < current->value) {
-				current = current->left;
-			}
-			else {
-				current = current->right;
-			}
-		}
+
+	void turn(Node*& current, Node* parent, bool isLeft) {
 		if (current == nullptr) return;
-		Node* child = current->right;
+		Node* child = nullptr;
+		if (isLeft) {
+			child = current->right;
+		}
+		else {
+			child = current->left;
+		}
 		if (child == nullptr) return;
 		if (parent == nullptr) {
 			root = child;
@@ -69,90 +70,31 @@ struct Tree {
 			if (parent->left == current) parent->left = child;
 			else parent->right = child;
 		}
-		current->right = child->left;
-		child->left = current;
+		if (isLeft) {
+			current->right = child->left;
+			child->left = current;
+		}
+		else {
+			current->left = child->right;
+			child->right = current;
+		}
+		return;
+	}
+	void ToLeft(int value) {
+		Node* parent = nullptr;
+		Node* current = goTo(value, parent);
+		turn(current, parent, true);
 	}
 	void ToRight(int value) {
 		Node* parent = nullptr;
-		Node* current = root;
-		while (current != nullptr) {
-			if (value == current->value) {
-				break;
-			}
-			parent = current;
-			if (value < current->value) {
-				current = current->left;
-			}
-			else {
-				current = current->right;
-			}
-		}
-		if (current == nullptr) return;
-		Node* child = current->left;
-		if (child == nullptr) return;
-		if (parent == nullptr) {
-			root = child;
-		}
-		else {
-			if (parent->left == current) parent->left = child;
-			else parent->right = child;
-		}
-		current->left = child->right;
-		child->right = current;
+		Node* current = goTo(value, parent);
+		turn(current, parent, false);
 	}
+
 	void Balance(Node* current = nullptr) {
-		if (current == nullptr) current = root;
-		if (current->left != nullptr) Balance(current->left);
-		if (current->right != nullptr) Balance(current->right);
-		if (subtreeDepth(current) < 3) return;
-		bool isLeft = true;
-		if (subtreeDepth(current->left) - 1 > subtreeDepth(current->right)) {
-			isLeft = true;
-			if (subtreeDepth(current->left->left) >= subtreeDepth(current->left->right)) {
-				//левая внешняя ветка самая глубокая
-				ToRight(current->value);
-			}
-			else {
-				//левая внутренняя ветка самая глубокая
-				ToLeft(current->left->value);
-				ToRight(current->value);
-			}
-		}
-		else if (subtreeDepth(current->left) < subtreeDepth(current->right) - 1) {
-			isLeft = false;
-			if (subtreeDepth(current->right->right) >= subtreeDepth(current->right->left)) {
-				//правая внешняя ветка самая глубокая
-				ToLeft(current->value);
-			}
-			else {
-				// правая внутренняя ветка самая глубокая
-				ToRight(current->right->value);
-				ToLeft(current->value);
-			}
-		}
-		//int* arr = ToArray(Postfix);
-		//for (int i = 0; i < count(); i++) {
-		//	cout << arr[i] << " ";
-		//}
-		//cout << endl;
-		while (subtreeDepth(current) > 3) {
-			if ((subtreeDepth(current) - minLeafDepth(current)) > 1) {
-				if (subtreeDepth(current->left) > subtreeDepth(current->right)) {
-					ToRight(current->value);
-				}
-				else {
-					ToLeft(current->value);
-				}
-			}
-			if (isLeft) {
-				current = current->right;
-			}
-			else {
-				current = current->left;
-			}
-		}
+
 	}
-	/*Node* goTo2(int value, Node*& parent) {
+	Node* goTo(int value, Node*& parent) {
 		Node* current = root;
 		while (current != nullptr) {
 			if (value == current->value) {
@@ -160,22 +102,6 @@ struct Tree {
 			}
 			parent = current;
 			if (value < current->value) {
-				current = current->left;
-			}
-			else {
-				current = current->right;
-			}
-		}
-		parent = nullptr;
-		return nullptr;
-	}*/
-	Node* goTo(int value) {
-		Node* current = root;
-		while (current != nullptr) {
-			if (value == current->value) {
-				return current;
-			}
-			else if (value < current->value) {
 				current = current->left;
 			}
 			else {
@@ -185,55 +111,28 @@ struct Tree {
 		return nullptr;
 	}
 	void add(int value) {
-		Node* newNode = new Node;
-		newNode->value = value;
-		size++;
-		if (count() == 1) {
+		if (!size) {
+			Node* newNode = new Node;
+			newNode->value = value;
+			size++;
 			root = newNode;
 			return;
 		}
-		Node* current = root;
-		while (true) {
-			if (value < current->value) {
-				if (current->left != nullptr) {
-					current = current->left;
-				}
-				else {
-					current->left = newNode;
-					return;
-				}
-			}
-			else if (value > current->value) {
-				if (current->right != nullptr) {
-					current = current->right;
-				}
-				else {
-					current->right = newNode;
-					return;
-				}
-			}
-			else if (value == current->value) {
-				delete newNode;
-				size--;
-				return;
-			}
+		Node* parent = nullptr;
+		if (goTo(value, parent) != nullptr) return;
+		Node* newNode = new Node;
+		newNode->value = value;
+		size++;
+		if (value > parent->value) {
+			parent->right = newNode;
+		}
+		else {
+			parent->left = newNode;
 		}
 	}
 	void remove(int value) {
 		Node* parent = nullptr;
-		Node* current = root;
-		while (current != nullptr) {
-			if (value == current->value) {
-				break;
-			}
-			parent = current;
-			if (value < current->value) {
-				current = current->left;
-			}
-			else {
-				current = current->right;
-			}
-		}
+		Node* current = goTo(value, parent);
 		if (current == nullptr) return;
 		if ((current->right != nullptr) && (current->left != nullptr)) {
 			parent = current;
@@ -278,7 +177,8 @@ struct Tree {
 		root = nullptr;
 	}
 	bool contains(int value) {
-		if (goTo(value) != nullptr) {
+		Node* parent = nullptr;
+		if (goTo(value, parent) != nullptr) {
 			return true;
 		}
 		else {
@@ -298,23 +198,7 @@ void main() {
 		tree.add(input);
 		cin >> input;
 	}
-	/*cout << tree.subtreeDepth(tree.root) << endl;
-	cout << tree.minLeafDepth(tree.root) << endl;
-
-	int* arr = tree.ToArray(Postfix);
-	for (int i = 0; i < tree.count(); i++) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-
-	tree.Balance();
-
-	arr = tree.ToArray(Postfix);
-	for (int i = 0; i < tree.count(); i++) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-	*/
+	
 	cout << "Enter numbers to search (sequence end sign 0):" << endl;
 	cin >> input;
 	while (input) {
